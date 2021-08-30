3a7823486fd5b9053fa0656d63872138914cedcc
Shaima Haque <shaimahaque02@yahoo.com>
2021-08-30 19:37:12 +0530

    Add Stack Data Structure

---
+++ stack_data_structure.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def get_stack(self):
+        return self.items
+
+
+
+myStack = Stack()
+myStack.push("A")
+myStack.push("B")
+print(myStack.get_stack())
+myStack.push("C")
+print(myStack.get_stack())
+myStack.pop()
+print(myStack.get_stack())
