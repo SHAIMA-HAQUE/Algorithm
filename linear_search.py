25e45492294153c7be7ee97fbfe7eebf9c08f1fe
Shaima Haque <shaimahaque02@yahoo.com>
2021-08-30 13:13:55 +0530

    Add Linear Search

---
+++ linear_search.py
@@ -0,0 +1,22 @@
+def main():
+    len = int(input("Enter the length of the list: "))
+    list = []
+    for i in range(len):
+        elem = int(input())
+        list.append(elem)
+    query = int(input("Enter the query: "))
+
+    print(locate_query(list,query))
+
+def locate_query(list,query):
+    position = 0
+    while True:
+        if list[position] == query:
+            return position
+        position += 1
+
+        if position == len(list):
+            return -1
+
+if __name__ == "__main__":
+    main()
