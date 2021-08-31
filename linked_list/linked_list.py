da82481c432186442cd5ff5f8145dc5106256731
Shaima Haque <shaimahaque02@yahoo.com>
2021-08-31 20:45:08 +0530

    Add print_list() in linkedlist

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -7,6 +7,11 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
+    def print_list(self):
+        curr_node = self.head
+        while curr_node:
+            print(curr_node.data)
+            curr_node = curr_node.next
 
     def append(self,data):
         new_node = Node(data)
@@ -17,3 +22,9 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
+
+
+linked_list = LinkedList()
+linked_list.append("A")
+linked_list.append("B")
+linked_list.print_list()
