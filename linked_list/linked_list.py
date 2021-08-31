6dfb6f48b690e651c147a341b844d72e075a511f
Shaima Haque <shaimahaque02@yahoo.com>
2021-08-31 20:41:38 +0530

    Add linked list with append function

---
+++ linked_list/linked_list.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+
+    def append(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
