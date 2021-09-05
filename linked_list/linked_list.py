0d704a80af3685f68320fd7ae14bda765bf89b4d
Shaima Haque <shaimahaque02@yahoo.com>
2021-09-05 18:45:22 +0530

    Add duplicate and merge

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -138,16 +138,94 @@ class LinkedList:
             return _reverse_recursive(curr_node,prev_node)
         self.head = _reverse_recursive(cur=self.head, prev=None)
 
+    def merge(self, llist):
+        p = self.head
+        q = llist.head
+        s = None
+
+        if not p:
+            return q
+        if not q:
+            return p
+        if p and q:
+            if p.data <= q.data:
+                s = p
+                p = s.next
+            else:
+                s = q
+                q = s.next
+            new_head = s
+        while p and q:
+            if p.data <= q.data:
+                s.next = p
+                s = p
+                p = s.next
+            else:
+                s.next = q
+                s = q
+                q = s.next
+        if not p:
+            s.next = q
+        if not q:
+            s.next = p
+        self.head = new_head
+        return self.head
+
+    def remove_duplicates(self):
+        curr_node = self.head
+        prev_node = None
+        dup_values = dict()
+
+        while curr_node:
+            if curr_node.data in dup_values:
+                prev_node.next = curr_node.next
+                curr_node.next = None
+            else:
+                dup_values[curr_node.data] = 1
+                prev_node = curr_node
+            curr_node = prev_node.next
 
 
-# 
+
+# llist = LinkedList()
+# llist.append("A")
+# llist.append("B")
+# llist.append("C")
+# llist.append("D")
+
+# llist.reverse_iterative()
+
+# llist.print_list()
+
+# llist_1 = LinkedList()
+# llist_2 = LinkedList()
+
+# llist_1.append(1)
+# llist_1.append(5)
+# llist_1.append(7)
+# llist_1.append(9)
+# llist_1.append(10)
+
+# llist_2.append(2)
+# llist_2.append(3)
+# llist_2.append(4)
+# llist_2.append(6)
+# llist_2.append(8)
+
+# llist_1.merge(llist_2)
+# llist_1.print_list()
 
 llist = LinkedList()
-llist.append("A")
-llist.append("B")
-llist.append("C")
-llist.append("D")
+llist.append(1)
+llist.append(6)
+llist.append(1)
+llist.append(4)
+llist.append(2)
+llist.append(2)
+llist.append(4)
 
-llist.reverse_iterative()
-
+print("Original Linked List")
+llist.print_list()
+print("Linked List After Removing Duplicates")
+llist.remove_duplicates()
 llist.print_list()
