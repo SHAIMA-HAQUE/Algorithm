28b6049065579d174587b395a0f239a23180ef7f
Shaima Haque <shaimahaque02@yahoo.com>
2022-01-20 23:56:58 +0530

    Stack Applications

--- stack and application/reverse_string_stack.py
+++ stack and application/reverse_string_stack.py
@@ -13,3 +13,18 @@ stack = Stack()
 input_str = "!dlroW olleH"
 print(reverse_string(stack, input_str))
     
+# import stack 
+
+# string = "!dlroW olleH"
+# reversed_string = ""
+# s = stack.Stack()
+
+# for char in string:
+#     s.push(char)
+    
+
+# while not s.is_empty():
+#     reversed_string += s.pop()
+
+
+# print(reversed_string)
